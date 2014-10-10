1186e6dfacbfe2384d9e7d92f88a4da0cfab01da
ciaranbaselmans <ciaranbaselmans@gmail.com>
2014-10-10 15:02:41 +0100

    Question 1 week 3 commit

---
+++ week2/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#: Title : week 3 EAA
+#: Date : 10/10/2014
+#: Author : Ciaran Baselmans
+#: Version : v1.0
+#: Description : week 3 questions 
+#: Options : - 
+
+mkdir $HOME/EAA/week2/bin
+mkdir $HOME/EAA/week2/scripts
+mv $HOME/EAA/week3/script.sh $HOME/EAA/week2/setup.sh
+
