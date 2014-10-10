c93fc1611583a508e513106fb0ac12661020f1ce
ciaranbaselmans <ciaranbaselmans@gmail.com>
2014-10-10 15:31:46 +0100

    Question 5 week 3

---
+++ week3/scripts/data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read name 
+echo "Please enter the month you were born in:"
+read month
+echo "Please enter your favorite colour:"
+read color
+
+echo "Hello $name you were born in $month and your favorite colour is $color"
